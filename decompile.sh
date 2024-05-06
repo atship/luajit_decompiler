b490235ac11f359cb81cc53b80b9403ce5f2d900
Hualin Song <ijsonlin@gmail.com>
2024-05-06 11:12:14 +0800

    Update decompile.sh

--- decompile.sh
+++ decompile.sh
@@ -1,2 +1,3 @@
 ## $1 is input dir, $2 is output dir
-python3 main.py -r $1 -d $2 -e lua,gui_script,render_script,script
+root=`dirname $0`
+python3 $root/main.py -r "$1" -d "$2" -e lua,gui_script,render_script,script
